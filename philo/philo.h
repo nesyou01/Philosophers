3f3e4776f9093a6c2cfee432f7886afd19c3ddc0
Youness Lagmah <lagmahraja@gmail.com>
2025-01-06 13:28:06 +0100

    Monitoring

--- philo/philo.h
+++ philo/philo.h
@@ -4,6 +4,8 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <limits.h>
+# include <pthread.h>
+#include <unistd.h>
 
 typedef struct s_vars
 {
@@ -13,6 +15,13 @@ typedef struct s_vars
 	size_t			time_to_sleep;
 	int				max_eat;
 }	t_vars;
+
+typedef struct s_philo
+{
+	int				nbr;
+	pthread_mutex_t	fork;
+	pthread_t		id;
+}	t_philo;
 
 int		ft_prase_vars(int argc, char **argv, t_vars *vars);
 size_t	ft_atoi(char *str, int *error);
